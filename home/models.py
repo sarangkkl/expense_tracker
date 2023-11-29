328d28386d7c8479ec2099515f8007e0945a5fe4
Gaurav Sah <sarangkkl2@gmail.com>
2023-11-29 19:47:32 +0530

    first commit

---
+++ home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+expense_type = (
+    ('Income', 'Income'),
+    ('Expense', 'Expense'),
+)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    expense_type = models.CharField(max_length=255, choices=expense_type)
+
+    def __str__(self):
+        return self.text + ' | ' + str(self.amount)
